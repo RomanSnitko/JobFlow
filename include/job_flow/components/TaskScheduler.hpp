ae369877164a5b7b716d59ab0384d86328f818fa
Roman Snitko <romsnitko@gmail.com>
2026-01-31 19:13:30 +0300

    add scheduler

--- include/job_flow/components/TaskScheduler.hpp
+++ include/job_flow/components/TaskScheduler.hpp
@@ -1,12 +1,28 @@
 #pragma once
-#include <userver/components/loggable_component_base.hpp>
 
-namespace job_flow::components {
-class TaskScheduler final : public userver::components::LoggableComponentBase {
+#include <userver/components/component_base.hpp>
+#include <userver/utils/periodic_task.hpp>
+#include <userver/storages/postgres/cluster.hpp>
+#include <userver/storages/redis/client.hpp>
+
+namespace job_flow::components 
+{
+
+class TaskScheduler final : public userver::components::ComponentBase 
+{
 public:
     static constexpr std::string_view kName = "task-scheduler";
 
     TaskScheduler(const userver::components::ComponentConfig& config,
-                 const userver::components::ComponentContext& context);
+                  const userver::components::ComponentContext& context);
+
+private:
+    void DoWork();
+
+    userver::storages::postgres::ClusterPtr pg_cluster_;
+    userver::storages::redis::ClientPtr redis_client_;
+    
+    userver::utils::PeriodicTask task_;
 };
-}
+
+} // namespace job_flow::components
--- src/components/TaskScheduler.cpp
+++ src/components/TaskScheduler.cpp
@@ -1,11 +1,40 @@
 #include <job_flow/components/TaskScheduler.hpp>
+#include <job_flow/storage/PostgresDAO.hpp>
+#include <job_flow/storage/RedisQueue.hpp>
 
-namespace job_flow::components {
+#include <userver/components/component_context.hpp>
+#include <userver/storages/postgres/component.hpp>
+#include <userver/storages/redis/component.hpp>
+
+namespace job_flow::components
+{
 
 TaskScheduler::TaskScheduler(const userver::components::ComponentConfig& config,
                              const userver::components::ComponentContext& context)
-    : LoggableComponentBase(config, context) {
-    // Здесь позже добавим PeriodicTask
+    : ComponentBase(config, context)
+    , pg_cluster_(context.FindComponent<userver::components::Postgres>("postgres-db-1").GetCluster())
+    , redis_client_(context.FindComponent<userver::components::Redis>("redis-tasks").GetClient("redis-tasks"))
+    , task_(
+          "scheduler-loop",
+          userver::utils::PeriodicTask::Settings{std::chrono::seconds(1)},
+          [this] { DoWork(); } 
+      )
+{
+
+}
+
+void TaskScheduler::DoWork()
+{
+    storage::PostgresDAO dao(pg_cluster_);
+    storage::RedisQueue queue(redis_client_);
+
+    std::vector<models::Task> tasks = dao.GetReadyTasks(100);
+
+    for (const auto& task : tasks) 
+    {
+        queue.Push(task.id);
+        dao.UpdateStatus(task.id, models::TaskStatus::kQueued);
+    }
 }
 
 } // namespace job_flow::components
