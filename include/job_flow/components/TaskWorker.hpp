b17c79b224a3dc0d8b6ebd20156d21d38474db86
Roman Snitko <romsnitko@gmail.com>
2026-01-31 19:30:49 +0300

    finish all modules and testing

--- include/job_flow/components/TaskWorker.hpp
+++ include/job_flow/components/TaskWorker.hpp
@@ -1,12 +1,32 @@
 #pragma once
-#include <userver/components/loggable_component_base.hpp>
 
-namespace job_flow::components {
-class TaskWorker final : public userver::components::LoggableComponentBase {
+#include <userver/components/component_base.hpp>
+#include <userver/engine/task/task.hpp>
+#include <userver/storages/postgres/cluster.hpp>
+#include <userver/storages/redis/client.hpp>
+
+namespace job_flow::components 
+{
+
+class TaskWorker final : public userver::components::ComponentBase 
+{
 public:
     static constexpr std::string_view kName = "task-worker";
 
     TaskWorker(const userver::components::ComponentConfig& config,
                const userver::components::ComponentContext& context);
+
+private:
+    void ProcessingLoop();
+
+    //imitation real task
+    void ExecuteTask(const std::string& task_id);
+
+    userver::storages::postgres::ClusterPtr pg_cluster_;
+    userver::storages::redis::ClientPtr redis_client_;
+    
+    //the object of process (coroutines)
+    userver::engine::Task worker_task_;
 };
-}
+
+} // namespace job_flow::components
--- src/components/TaskWorker.cpp
+++ src/components/TaskWorker.cpp
@@ -1,11 +1,59 @@
 #include <job_flow/components/TaskWorker.hpp>
+#include <job_flow/storage/PostgresDAO.hpp>
+#include <job_flow/storage/RedisQueue.hpp>
 
-namespace job_flow::components {
+#include <userver/components/component_context.hpp>
+#include <userver/storages/postgres/component.hpp>
+#include <userver/storages/redis/component.hpp>
+#include <userver/engine/sleep.hpp>
+
+namespace job_flow::components 
+{
 
 TaskWorker::TaskWorker(const userver::components::ComponentConfig& config,
                        const userver::components::ComponentContext& context)
-    : LoggableComponentBase(config, context) {
-    // Здесь позже добавим цикл обработки задач
+    : ComponentBase(config, context)
+    , pg_cluster_(context.FindComponent<userver::components::Postgres>("postgres-db-1").GetCluster())
+    , redis_client_(context.FindComponent<userver::components::Redis>("redis-tasks").GetClient("redis-tasks"))
+{
+    //inf cycle in coroutine
+    worker_task_ = userver::engine::AsyncNoSpan([this] { ProcessingLoop(); });
+}
+
+void TaskWorker::ProcessingLoop()
+{
+    storage::RedisQueue queue(redis_client_);
+
+    while (!userver::engine::current_task::ShouldCancel()) 
+    {
+        std::optional<std::string> task_id_opt = queue.Pop();
+
+        if (task_id_opt) 
+        {
+            ExecuteTask(*task_id_opt);
+        } 
+        else 
+        {
+            //sleep 100ms
+            userver::engine::SleepFor(std::chrono::milliseconds(100));
+        }
+    }
+}
+
+void TaskWorker::ExecuteTask(const std::string& task_id)
+{
+    storage::PostgresDAO dao(pg_cluster_);
+
+    dao.UpdateStatus(task_id, models::TaskStatus::kRunning);
+
+    //imitation task
+    LOG_INFO() << "WORKER IS DOING TASK: " << task_id;
+    
+    //execution 700ms by 1 task.. on average)))
+    userver::engine::SleepFor(std::chrono::milliseconds(700));
+
+    dao.UpdateStatus(task_id, models::TaskStatus::kCompleted);
+    LOG_INFO() << "SUCCESSFULLY FINISHED TASK: " << task_id;
 }
 
 } // namespace job_flow::components
